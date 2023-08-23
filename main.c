210c0706c6060fe8695e35ea51eb10e2be42ebf9
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2023-08-23 17:22:46 -0300

    rope head follows mouse position

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #include "raylib.h"
 #include "custom_rendering.h"
 
+const int WINDOW_WIDTH = 1200;
+const int WINDOW_HEIGHT = 800;
+const char WINDOW_TITLE[] = "Rope simulation";
+
 struct RopeNode {
     float x;
     float y;
@@ -65,61 +69,87 @@ void render_rope_nodes(struct RopeNode *node) {
             draw_rectangle(
                 (Vector2){ node->x, node->y },
                 (Vector2){ node->nextNode->x, node->nextNode->y },
-                30,
+                20,
                 WHITE
             );
+            DrawCircle(node->x, node->y, 20, RED);
+        } else {
+            DrawCircle(node->x, node->y, 20, GREEN);
         }
-
-        DrawCircle(node->x, node->y, 20, RED);
 
         node = node->nextNode;
     }
 }
 
-const int WINDOW_WIDTH = 1200;
-const int WINDOW_HEIGHT = 800;
-const char WINDOW_TITLE[] = "Rope simulation";
+void test_func(struct RopeNode **node) {
+    struct RopeNode *head = malloc(sizeof(struct RopeNode));
+    struct RopeNode *tail = malloc(sizeof(struct RopeNode));
+
+    head->x = 400;
+    head->y = 400;
+
+    tail->x = 300;
+    tail->y = 300;
+
+    tail->nextNode = head;
+
+    *node = tail;
+}
+
+void head_update(struct RopeNode **tail) {
+    Vector2 mousePosition = GetMousePosition();
+
+    (*tail)->nextNode->x = mousePosition.x;
+    (*tail)->nextNode->y = mousePosition.y;
+
+    float distance = distance_between_adjacent_nodes((*tail));
+
+    // TODO apply forces to tail.
+}
 
 int main() {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
     struct RopeNode *firstNode = NULL;
+    test_func(&firstNode);
 
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
-            Vector2 mousePosition = GetMousePosition();
+        head_update(&firstNode);
 
-            if (firstNode == NULL) {
-                firstNode = malloc(sizeof(struct RopeNode));
+        /* if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) { */
+        /*     Vector2 mousePosition = GetMousePosition(); */
 
-                firstNode->x = mousePosition.x;
-                firstNode->y = mousePosition.y;
-                firstNode->nextNode = NULL;
+        /*     if (firstNode == NULL) { */
+        /*         firstNode = malloc(sizeof(struct RopeNode)); */
 
-                /* system("clear"); */
-                /* print_list(firstNode); */
-            } else {
-                struct RopeNode *lastNode = get_last_rope_node(firstNode);
-                struct RopeNode *newNode = malloc(sizeof(struct RopeNode));
+        /*         firstNode->x = mousePosition.x; */
+        /*         firstNode->y = mousePosition.y; */
+        /*         firstNode->nextNode = NULL; */
 
-                newNode->x = mousePosition.x;
-                newNode->y = mousePosition.y;
-                newNode->nextNode = NULL;
+        /*         /\* system("clear"); *\/ */
+        /*         /\* print_list(firstNode); *\/ */
+        /*     } else { */
+        /*         struct RopeNode *lastNode = get_last_rope_node(firstNode); */
+        /*         struct RopeNode *newNode = malloc(sizeof(struct RopeNode)); */
 
-                lastNode->nextNode = newNode;
+        /*         newNode->x = mousePosition.x; */
+        /*         newNode->y = mousePosition.y; */
+        /*         newNode->nextNode = NULL; */
 
-                /* system("clear"); */
-                /* print_list(firstNode); */
-            }
-        }
+        /*         lastNode->nextNode = newNode; */
 
-        if (IsKeyPressed(KEY_C)) {
-            clear_nodes(firstNode);
-            firstNode = NULL;
-        }
+        /*         /\* system("clear"); *\/ */
+        /*         /\* print_list(firstNode); *\/ */
+        /*     } */
+        /* } */
+
+        /* if (IsKeyPressed(KEY_C)) { */
+        /*     clear_nodes(firstNode); */
+        /*     firstNode = NULL; */
+        /* } */
 
         render_rope_nodes(firstNode);
 
@@ -127,7 +157,6 @@ int main() {
     }
 
     free(firstNode);
-
     CloseWindow();
 
     return 0;
